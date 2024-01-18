1cdf65d896d0d9991616d74cfe1da338ad65d003
advaypakhale <advay.pakhale@gmail.com>
2024-01-18 14:33:08 +0800

    Update task_state.py

--- tasks/tasks/task_state.py
+++ tasks/tasks/task_state.py
@@ -45,7 +45,7 @@ class TaskState(Node):
 
         # Initialize self.state
         while (self.state is None) or (self.state is None):
-            rclpy.spin(self)
+            rclpy.spin_once(self)
 
     def _on_receive_state(self, msg):
         self.state = msg
